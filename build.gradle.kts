0a93545679eceffa978d422effc16215258ebf8e
DarviL <davidlosantos89@gmail.com>
2023-11-26 19:29:34 +0100

    update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.darvil"
-version = "0.0.1"
+version = "0.0.2"
 description = "Text formatting utilities to easily format text on the terminal for Java."
 
 repositories {
