97a3f316c6f122ae3cc5b948d466b8e8110d3f53
darvil82 <davidlosantos89@gmail.com>
2024-03-25 17:02:24 +0100

    feat: now any value can be concatenated to a TextFormatter

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 group = "com.darvil"
-version = "2.1.0"
+version = "2.2.0"
 description = "Text formatting utilities to easily format text on the terminal for Java."
 
 dependencies {
-    implementation("com.darvil:utils:+")
+    implementation("com.darvil:utils:0.6.1")
 
     implementation("org.jetbrains:annotations:24.0.0")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
